433b6d9401b261dc34c3ea81de43644d643cd5a8
CHAEWONSEOK98 <7dugod7@naver.com>
2022-01-31 20:11:03 +0900

    typescript theme

--- master-cra/src/App.tsx
+++ master-cra/src/App.tsx
@@ -1,29 +1,18 @@
-import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const App = () => {
-  const [value, setValue] = useState("");
-  function handleInputChange(event: React.FormEvent<HTMLInputElement>) {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  }
-  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          value={value}
-          onChange={handleInputChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>login</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 };
 
