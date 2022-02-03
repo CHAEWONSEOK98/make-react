1b85b4d531de2d057e2176ec80e55318118892c1
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-03 15:49:42 +0900

    Coin.tsx > react-query practice

--- master-cra/src/App.tsx
+++ master-cra/src/App.tsx
@@ -1,5 +1,6 @@
 import Router from "./Router";
 import { createGlobalStyle } from "styled-components";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const GlobalStyle = createGlobalStyle`
 @import url('https://fonts.googleapis.com/css2?family=Comforter&family=Source+Sans+Pro:wght@300&display=swap');
@@ -70,6 +71,7 @@ const App = () => {
     <>
       <GlobalStyle />
       <Router />
+      <ReactQueryDevtools initialIsOpen={true} />
     </>
   );
 };
