ebf107b5066b5a392305a7165bc30295d90b77e4
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-07 00:02:48 +0900

    chart2

--- master-cra/src/api.ts
+++ master-cra/src/api.ts
@@ -27,8 +27,8 @@ export function fetchCoinTickers(coinId: string) {
 
 export function fetchCoinHistory(coinId: string) {
   const endDate = Math.floor(Date.now() / 1000);
-  const startDate = (endDate - 60) ^ (60 * 24 * 7); //7일전.
+  const startDate = endDate - 60 * 60 * 24 * 7 * 2; //7 * 2일전.
   return fetch(
-    `${BASE_URL}/coinss/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+    `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
   ).then((response) => response.json());
 }
