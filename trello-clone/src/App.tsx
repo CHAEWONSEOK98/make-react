3c567325d86ec49f305443cc553d35a9c186058f
CHAEWONSEOK98 <7dugod7@naver.com>
2022-02-22 15:40:58 +0900

    get selectors

---
+++ trello-clone/src/App.tsx
@@ -0,0 +1,24 @@
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+
+function App() {
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+    // + 를 붙여줌으로 string > number
+  };
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" />
+    </div>
+  );
+}
+
+export default App;
